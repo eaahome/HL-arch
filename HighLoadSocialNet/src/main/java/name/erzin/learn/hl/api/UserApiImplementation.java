47a446b473d867bf29b4ce4ebb6fe2f2bfa34b97
Alex Erzin <erzinaa@srgroup.ru>
2025-09-10 23:33:36 +0300

    Create new user using native SQL

--- HighLoadSocialNet/src/main/java/name/erzin/learn/hl/api/UserApiImplementation.java
+++ HighLoadSocialNet/src/main/java/name/erzin/learn/hl/api/UserApiImplementation.java
@@ -9,26 +9,49 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @Component
 public class UserApiImplementation implements UserApiDelegate {
+    ModelMapper modelMapper = new ModelMapper();
     @Autowired
     UserRepo userRepo;
 
     @Override
     public ResponseEntity<UserRegisterPost200Response> userRegisterPost(UserRegisterPostRequest userRegisterPostRequest) {
-        return UserApiDelegate.super.userRegisterPost(userRegisterPostRequest);
+        // Convert request to Entity
+        name.erzin.learn.hl.entity.User user = modelMapper.map(userRegisterPostRequest,
+                name.erzin.learn.hl.entity.User.class);
+        user.setId(UUID.randomUUID().toString());
+
+        insertUserNative(user);
+
+        UserRegisterPost200Response response = new UserRegisterPost200Response();
+        response.setUserId(user.getId());
+        return new ResponseEntity<>(response, HttpStatusCode.valueOf(200));
+    }
+
+    @Deprecated
+    private void insertUser (name.erzin.learn.hl.entity.User user) {
+        // This at first do SELECT and then INSERT
+        userRepo.save(user);
+    }
+
+    private void insertUserNative (name.erzin.learn.hl.entity.User user) {
+        userRepo.insertUser(user.getId(), user.getFirstName(), user.getSecondName(), user.getBirthdate().toString(),
+                user.getSex(), user.getBiography(), user.getCity());
     }
 
     @Override
     public ResponseEntity<User> userGetIdGet(String id) {
-        name.erzin.learn.hl.entity.User user = userRepo.findById(id);
+        Optional<name.erzin.learn.hl.entity.User> user = userRepo.findById(id);
 
-        if (user != null) {
+        if (user.isPresent()) {
             // Just map Entity to DTO
-            ModelMapper mm = new ModelMapper();
-            User userDTO = mm.map(user, User.class);
-
+            User userDTO = modelMapper.map(user.get(), User.class);
             return new ResponseEntity<>(userDTO, HttpStatusCode.valueOf(200));
         } else {
             return new ResponseEntity<>(null, HttpStatusCode.valueOf(404));
--- HighLoadSocialNet/src/main/java/name/erzin/learn/hl/repository/UserRepo.java
+++ HighLoadSocialNet/src/main/java/name/erzin/learn/hl/repository/UserRepo.java
@@ -1,16 +1,27 @@
 package name.erzin.learn.hl.repository;
 
 
+import jakarta.transaction.Transactional;
 import name.erzin.learn.hl.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
-public interface UserRepo extends JpaRepository<User, Long> {
+public interface UserRepo extends JpaRepository<User, String> {
     @Query(value = "SELECT * FROM user", nativeQuery = true)
     ArrayList<User> findAll();
 
     @Query(value = "SELECT * FROM user where id = :id", nativeQuery = true)
-    User findById (String id);
+    Optional<User> findById (String id);
+
+    @Transactional
+    @Modifying
+    @Query(value = "INSERT INTO user (id, first_name, second_name, birthdate, sex, biography, city)"
+            + " VALUES (:id, :firstName, :secondName, :birthdate, :sex, :biography, :city)",
+            nativeQuery = true)
+    void insertUser (String id, String firstName, String secondName, String birthdate, String sex, String biography,
+                     String city);
 }
