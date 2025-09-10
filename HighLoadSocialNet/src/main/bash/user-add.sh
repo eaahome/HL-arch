b346261bb35e4af4e7aa1022921ec6af49012c54
Alex Erzin <erzinaa@srgroup.ru>
2025-09-10 23:33:45 +0300

    Create new user using native SQL

---
+++ HighLoadSocialNet/src/main/bash/user-add.sh
@@ -0,0 +1,14 @@
+curl -X 'POST' \
+  'http://localhost:8080/user/register' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "first_name": "Имя",
+  "second_name": "Фамилия",
+  "birthdate": "2017-02-01",
+  "biography": "Хобби, интересы и т.п.",
+  "city": "Москва",
+  "sex": "Секретная строка"
+}'
+
+echo
