9d746335585c7ad552d909ce1fb676af6e644a31
Alex Erzin <erzinaa@srgroup.ru>
2025-09-21 09:06:42 +0300

    Moved to Postgres instead of Mysql

--- HighLoadSocialNet/src/main/java/name/erzin/learn/hl/repository/UserRepo.java
+++ HighLoadSocialNet/src/main/java/name/erzin/learn/hl/repository/UserRepo.java
@@ -11,15 +11,15 @@ import java.util.ArrayList;
 import java.util.Optional;
 
 public interface UserRepo extends JpaRepository<User, String> {
-    @Query(value = "SELECT * FROM user", nativeQuery = true)
+    @Query(value = "SELECT * FROM \"user\"", nativeQuery = true)
     ArrayList<User> findAll();
 
-    @Query(value = "SELECT * FROM user where id = :id", nativeQuery = true)
+    @Query(value = "SELECT * FROM \"user\" where id = :id", nativeQuery = true)
     Optional<User> findById (String id);
 
     @Transactional
     @Modifying
-    @Query(value = "INSERT INTO user (id, first_name, second_name, birthdate, sex, biography, city)"
+    @Query(value = "INSERT INTO \"user\" (id, first_name, second_name, birthdate, sex, biography, city)"
             + " VALUES (:id, :firstName, :secondName, :birthdate, :sex, :biography, :city)",
             nativeQuery = true)
     void insertUser (String id, String firstName, String secondName, String birthdate, String sex, String biography,
---
+++ HighLoadSocialNet/src/main/resources/createdb.postgres.sql
@@ -0,0 +1,23 @@
+DROP TABLE public."user";
+CREATE TABLE IF NOT EXISTS public."user" (
+    id character varying NOT NULL,
+    first_name character varying NOT NULL,
+    second_name character varying NOT NULL,
+    birthdate character varying NOT NULL,
+    sex character varying NOT NULL,
+    biography character varying NOT NULL,
+    city character varying NOT NULL
+);
+
+ALTER TABLE public."user" OWNER TO hl1;
+
+insert into "user" values ('user1', 'FIRST', 'SECOND', '1978-05-04', 'SEX', 'BIO', 'MSK');
+insert into "user" values ('user2', 'FIRST2', 'SECOND2', '1978-05-04', 'SEX2', 'BIO2', 'MSK2');
+
+CREATE TABLE IF NOT EXISTS account (
+    login varchar(255) NOT NULL primary key,
+    password varchar(255)
+);
+
+insert into account values ('user1', '8DUJw3vDAUsBQnBYFwmnmg==');
+insert into account values ('user2', '123');
