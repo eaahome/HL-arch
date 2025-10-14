d383318dab27fb84026c9eae4b7da16557a65a79
Alex Erzin <erzinaa@srgroup.ru>
2025-10-14 09:02:34 +0300

    Logs added

--- HighLoadSocialNet/src/main/java/name/erzin/learn/hl/service/CachedUserFeed.java
+++ HighLoadSocialNet/src/main/java/name/erzin/learn/hl/service/CachedUserFeed.java
@@ -1,11 +1,13 @@
 package name.erzin.learn.hl.service;
 
 import lombok.Getter;
+import lombok.extern.java.Log;
 import name.erzin.learn.hl.model.Post;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Log
 @Getter
 public class CachedUserFeed {
     private List<Post> posts;
@@ -23,8 +25,10 @@ public class CachedUserFeed {
     public void addPost (Post post) {
         posts.add(0, post); // Latest post must be first in list
         if (posts.size() > FEED_MAX_SIZE) {
+            log.info ("Cache too large, remove last element");
             posts.remove(posts.size() - 1); // Remove last
         }
+        log.info("Cache size after adding: " + posts.size());
     }
 
     public void deletePost(String postId) {
@@ -32,6 +36,7 @@ public class CachedUserFeed {
         for (Post post : posts) {
             if (post.getId().equals(postId)) {
                 posts.remove(i);
+                log.info("Remove Post " + postId + " from cache, cache size: " + posts.size());
                 return;
             }
             i++;
