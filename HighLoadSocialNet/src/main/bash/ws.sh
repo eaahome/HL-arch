c59a3c6850721fbd80a8bcc2d04c99cdcd9b4146
Alex Erzin <erzinaa@srgroup.ru>
2025-11-06 00:00:55 +0300

    WebSocket test tool

---
+++ HighLoadSocialNet/src/main/bash/ws.sh
@@ -0,0 +1,3 @@
+wscat \
+ -H 'Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3NjIzNjc5NDQsImV4cCI6MTc2NDk1OTk0NCwibG9naW4iOiJ1c2VyMSJ9.IbZPb1yS_usQvJyc4GG9D04uSsC483IHFjbIpIRFfcM' \
+ --connect localhost:8080/post/feed/posted
